63bd516e5efc748b48fc2ccc08d5d4cff6184dfb
John Meek <jrmeek@amazon.com>
2024-03-05 17:08:16 -0700

    Introductory commit with code from various internet sources listed below.
    This will detect message and print out IP addresses for WIFI management packets.
    
    The code was a bit outdated so minor tweaks were needed to just get it running.
    
    Sources:
    
    https://docs.espressif.com/projects
    https://github.com/lpodkalicki/blog/blob/master/esp32/016_wifi_sniffer/main/main.c

---
+++ main/scan.c
@@ -0,0 +1,155 @@
+/* Scan Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+/*
+    This example shows how to scan for available set of APs.
+*/
+#include <string.h>
+//#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_wifi_types.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+
+#define	LED_GPIO_PIN GPIO_NUM_4
+#define	WIFI_CHANNEL_MAX 13
+#define	WIFI_CHANNEL_SWITCH_INTERVAL 500
+
+/* #define DEFAULT_SCAN_LIST_SIZE CONFIG_EXAMPLE_SCAN_LIST_SIZE */
+#define DEFAULT_SCAN_LIST_SIZE 20
+
+static wifi_country_t wifi_country = {.cc="US", .schan=1, .nchan=13, .policy=WIFI_COUNTRY_POLICY_AUTO};
+
+typedef struct {
+	unsigned frame_ctrl:16;
+	unsigned duration_id:16;
+	uint8_t addr1[6]; /* receiver address */
+	uint8_t addr2[6]; /* sender address */
+	uint8_t addr3[6]; /* filtering address */
+	unsigned sequence_ctrl:16;
+	uint8_t addr4[6]; /* optional */
+} wifi_ieee80211_mac_hdr_t;
+
+typedef struct {
+	wifi_ieee80211_mac_hdr_t hdr;
+	uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
+} wifi_ieee80211_packet_t;
+
+//static esp_err_t event_handler(void *ctx, system_event_t *event);
+static void wifi_sniffer_init(void);
+static void wifi_sniffer_set_channel(uint8_t channel);
+static const char *wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type);
+static void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type);
+
+//static const char *TAG = "scan";
+
+void app_main(void)
+{
+	uint8_t channel = 1;
+	//uint8_t level = 0;
+
+	/* setup */
+	wifi_sniffer_init();
+	//gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
+
+	/* loop */
+    // Add code to recieve IO input to cancel and exit routine... 
+	while (true) {
+		//gpio_set_level(LED_GPIO_PIN, level ^= 1);
+		vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
+		wifi_sniffer_set_channel(channel);
+		channel = (channel % WIFI_CHANNEL_MAX) + 1;
+    }
+
+    for (int i = 3; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
+
+//esp_err_t event_handler(void *ctx, system_event_t *event)
+//{	
+//	return ESP_OK;
+//}
+
+void wifi_sniffer_init(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+//    	tcpip_adapter_init();
+//    	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+	ESP_ERROR_CHECK( esp_wifi_set_country(&wifi_country) ); /* set country for channel range [1, 13] */
+	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
+    	ESP_ERROR_CHECK( esp_wifi_start() );
+	esp_wifi_set_promiscuous(true);
+	esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
+}
+
+void wifi_sniffer_set_channel(uint8_t channel)
+{
+	esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+}
+
+const char * wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type)
+{
+	switch(type) {
+		case WIFI_PKT_MGMT: return "MGMT";
+		case WIFI_PKT_DATA: return "DATA";
+		default:	
+		case WIFI_PKT_MISC: return "MISC";
+	}
+}
+
+void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
+{
+
+	if (type != WIFI_PKT_MGMT)
+		return;
+
+	const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
+	const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
+	const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
+
+	printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
+		" ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+		" ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+		" ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
+		wifi_sniffer_packet_type2str(type),
+		ppkt->rx_ctrl.channel,
+		ppkt->rx_ctrl.rssi,
+		/* ADDR1 */
+		hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
+		hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
+		/* ADDR2 */
+		hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
+		hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
+		/* ADDR3 */
+		hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
+		hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
+	);
+}
+
+
+
+
